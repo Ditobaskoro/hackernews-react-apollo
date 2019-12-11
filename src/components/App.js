3427a54762ec696848b06613655096788df4eb11
Dito Baskoro <dito.anwar@yahoo.com>
2019-12-11 18:00:54 +0700

    Add filter links

--- src/components/App.js
+++ src/components/App.js
@@ -6,6 +6,7 @@ import '../styles/App.css';
 import LinkList from './LinkList'
 import CreateLink from './CreateLink'
 import Login from './Login'
+import Search from './Search'
 
 function App() {
   return (
@@ -16,6 +17,7 @@ function App() {
           <Route exact path="/" component={LinkList} />
           <Route exact path="/create" component={CreateLink} />
           <Route exact path="/login" component={Login} />
+          <Route exact path='/search' component={Search} />
         </Switch>
       </div>
     </div>
